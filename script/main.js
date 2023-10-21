12c1f980d25f93175a93b4bdc0e45658422b313f
VladM <vdstories20@gmail.com>
2023-10-21 23:55:16 +0200

    added rerposive page + mobile menu

--- script/main.js
+++ script/main.js
@@ -1,13 +1,28 @@
+// mobile menu
+
+const pwOpenModalBtn = document.querySelector(".pw-header-burger-button");
+const pwOpenModal = document.querySelector(".pw-header-container");
+const pwCloseModalBtn = document.querySelector(".pw-header-close-btn");
+
+pwOpenModalBtn.addEventListener("click", SsToggleModal);
+pwCloseModalBtn.addEventListener("click", SsToggleModal);
+
+function SsToggleModal() {
+  pwOpenModal.classList.toggle("hidden");
+}
+
+// disable download picture
+
 let pwAllImages = document.querySelectorAll("img");
 pwAllImages.forEach((value) => {
-    value.oncontextmenu = (evt) => {
-        evt.preventDefault();
-    }
+  value.oncontextmenu = (evt) => {
+    evt.preventDefault();
+  };
 });
 
 let pwAllVector = document.querySelectorAll("svg");
 pwAllVector.forEach((value) => {
-    value.oncontextmenu = (evt) => {
-        evt.preventDefault();
-    }
+  value.oncontextmenu = (evt) => {
+    evt.preventDefault();
+  };
 });
