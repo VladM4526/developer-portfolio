0e4dd2838e6df1ecbfb39a3a6314ef57c695c979
VladM <vdstories20@gmail.com>
2023-10-22 02:38:41 +0200

    added job landing page

--- script/main.js
+++ script/main.js
@@ -4,11 +4,20 @@ const pwOpenModalBtn = document.querySelector(".pw-header-burger-button");
 const pwOpenModal = document.querySelector(".pw-header-container");
 const pwCloseModalBtn = document.querySelector(".pw-header-close-btn");
 
+const pwItemList = document.querySelector(".pw-header-list-item");
+const pwItemLink = document.querySelector(".pw-header-list-item-link");
+
 pwOpenModalBtn.addEventListener("click", SsToggleModal);
 pwCloseModalBtn.addEventListener("click", SsToggleModal);
 
 function SsToggleModal() {
   pwOpenModal.classList.toggle("hidden");
+}
+
+pwItemLink.addEventListener("click", closeMenu);
+
+function closeMenu() {
+  pwOpenModal.classList.remove("hidden");
 }
 
 // disable download picture
